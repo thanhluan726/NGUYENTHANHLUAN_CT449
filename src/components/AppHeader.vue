41599236313c833d62838b4edeb5eee02f7f4822
thanhluan726 <113674240+thanhluan726@users.noreply.github.com>
2022-11-04 15:37:45 +0700

    abc

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/" class="navbar-brand">Ứng dụng Quản lý danh bạ</a>
+        <div class="mr-auto navbar-nav">
+            <li class="nav-item">
+
+                <router-link :to="{ name: 'contactbook' }" class="nav-link">
+                    Danh bạ
+                    <i class="fas fa-address-book"></i>
+    
+                </router-link>
+    
+             </li>
+         </div>
+    </nav>
+</template>
